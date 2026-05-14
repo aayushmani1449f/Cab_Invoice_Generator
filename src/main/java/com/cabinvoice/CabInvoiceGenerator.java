9d4637c5caa6af295378da31bbc11750f56b408f
Aayush Mani <aayushmani1449f@gmail.com>
2026-05-14 11:46:40 +0530

    UC1 - Calculate Cab Fare

---
+++ src/main/java/com/cabinvoice/CabInvoiceGenerator.java
@@ -0,0 +1,12 @@
+package com.cabinvoice;
+
+public class CabInvoiceGenerator {
+    private static final double COST_PER_KM = 10.0;
+    private static final int COST_PER_MINUTE = 1;
+    private static final double MINIMUM_FARE = 5.0;
+
+    public double calculateFare(double distance, int time) {
+        double totalFare = distance * COST_PER_KM + time * COST_PER_MINUTE;
+        return Math.max(totalFare, MINIMUM_FARE);
+    }
+}
